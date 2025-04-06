c94c0de22a9da77616724d8aac461fa9b085081e
SanchezZz-z <anikitin278@gmail.com>
2025-04-06 18:36:40 +0300

    Removed redundant router

--- tgbot_template_v3/tgbot/handlers/__init__.py
+++ tgbot_template_v3/tgbot/handlers/__init__.py
@@ -1,7 +1,6 @@
 """Import all routers and add them to routers_list."""
 from .admin import admin_router
 from .echo import echo_router
-from .simple_menu import menu_router
 from .user import user_router
 from .train_menu import train_menu_router
 from .monitor_menu import monitor_menu_router
